de7ec229dd6d670722b88199f8127c12f1d809df
whynot1992 <vadym.sokolov1992@gmail.com>
2025-01-29 14:04:26 +0200

    add new homework

---
+++ Python3_Basic_Hillel/1_lesson/data_types.py
@@ -0,0 +1,21 @@
+first_name = "one"
+second_name = 13
+third_naem = "my next text"
+four_value = 132
+five_value = 25.4
+six_value = "hello world!"
+seven_name = "I love Python3"
+eight_name = 11983921
+nine_name = 312323.3213
+ten_name = "one, two, three"
+
+print(first_name, type(first_name))
+print(second_name, type(second_name))
+print(third_naem, type(third_naem))
+print(four_value, type(four_value))
+print(five_value, type(five_value))
+print(six_value, type(six_value))
+print(seven_name, type(seven_name))
+print(eight_name, type(eight_name))
+print(nine_name, type(nine_name))
+print(ten_name, type(ten_name))
