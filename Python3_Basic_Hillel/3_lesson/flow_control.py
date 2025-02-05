d6d2d10b5c3867a417b0260c9f4d70cffa40839b
whynot <vadym.sokolov1992@gmail.com>
2025-02-05 15:10:29 +0200

    update

--- Python3_Basic_Hillel/3_lesson/flow_control.py
+++ Python3_Basic_Hillel/3_lesson/flow_control.py
@@ -1,18 +1,9 @@
 def checking_value(row_value):
-    try:
-        if bool(float(row_value)) or row_value.isdigit():
-            return print("All OK")
-    except ValueError:
-        return print("You made a mistake in the water, a real or integer number " \
-              "should be written here")
-         
-        
+        pass        
 
 def main():
     first_value = checking_value(input("Write here your first number: "))
-    # second_value = checking_value(input("Write here your second number: "))
-    # return print(first_value + second_value)
-    return first_value
+    return print(first_value)
 
 if __name__ == "__main__":
     main()
