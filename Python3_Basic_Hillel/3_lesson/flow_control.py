d46cc00d265f60b6ab798862262030d830271b52
whynot1992 <vadym.sokolov1992@gmail.com>
2025-02-06 19:46:14 +0200

    made homework

--- Python3_Basic_Hillel/3_lesson/flow_control.py
+++ Python3_Basic_Hillel/3_lesson/flow_control.py
@@ -1,9 +1,48 @@
-def checking_value(row_value):
-        pass        
+# We get the first value and figure it out
 
-def main():
-    first_value = checking_value(input("Write here your first number: "))
-    return print(first_value)
+try:
+    first_value = input("Write here your first number: ")
+    
+    if first_value.isdigit():
+        first_value = int(first_value)
+    else:
+        try:
+            first_value = float(first_value) if bool(float(first_value)) else first_value
+        except ValueError:
+            raise ValueError("Input is neither an integer or a valid float.")
+except ValueError as e:
+    print(f"Error: {e}")
 
-if __name__ == "__main__":
-    main()
+# We get the second value and figure it out
+
+try:
+    second_value = input("Write here your first number: ")
+    
+    if second_value.isdigit():
+        second_value = int(second_value)
+    else:
+        try:
+            second_value = float(second_value) if bool(float(first_value)) else second_value
+        except ValueError:
+            raise ValueError("Input is neither an integer or a valid float.")
+except ValueError as e:
+    print(f"Error: {e}")
+
+# We get operants and make action
+
+action = input("Enter a sign from the list +, -, *, /:")
+
+if action not in ["+", "-", "*", "/"]:
+    print("You make mistake! You need to type a sing from the list +, -, *, /.")
+else:
+    if action == "+":
+        print(f"Your answer: {first_value} {action} {second_value} = {first_value + second_value}")
+    elif action == "-":
+        print(f"Your answer: {first_value} {action} {second_value} = {first_value - second_value}")
+    elif action == "*":
+        print(f"Your answer: {first_value} {action} {second_value} = {first_value * second_value}")
+    elif action == "/":
+        try:
+            print(f"Your answer: {first_value} {action} {second_value} = {first_value / second_value}")
+        except ZeroDivisionError:
+            print("Cannot divide by zero!")
