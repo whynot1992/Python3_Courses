d431d1b3065b0283506692967769ec2d6ccd654f
whynot1992 <vadym.sokolov1992@gmail.com>
2025-02-10 18:02:52 +0200

    add new 6_lesson and homework

--- Python3_Basic_Hillel/6_lesson/dicts.py
+++ Python3_Basic_Hillel/6_lesson/dicts.py
@@ -2,6 +2,10 @@
 #       - Використай генератор словника
 #       - Використай вбудовану функцію chr()
 
+ascii_table = {i: chr(i) for i in range(128)}
+
+print(ascii_table)
+
 # 2.Реалізувати послідовність операцій яка
 #       - Приймає від користувача строку тільки з літер, без пробілів
 #       - Приймає від користувача пароль у вигляді цілого числа
@@ -12,3 +16,34 @@
 #               + З паролем у 4 порядок стає "a" - 5, "b" - 6, ..., "z" - 4 -> словник "a"="e", "b"="f", ..., "z"="d"
 #               + Рядок "hello" перетворюється на "lipps"
 #       - Друкує зашифровану строку
+
+import sys
+
+
+alphabet = "abcdefghijklmnopqrstuvwxyz"
+alphabet_dict = {letter: index for index, letter in enumerate(alphabet, start=1)}
+reversed_alphabet = {v: k for k, v in alphabet_dict.items()}
+
+user_text = input("Write here your text: ").lower()
+
+if user_text.isalpha():
+    user_password = input("Write here your Integer number: ")
+
+    try:
+        row_output = list()
+
+        if int(user_password):
+
+            for char in user_text:
+                new_index = (alphabet_dict[char] + int(user_password) - 1) % 26 + 1
+                row_output.append(reversed_alphabet[new_index])
+
+        output = ''.join(row_output)
+        print(output)
+
+    except ValueError as error:
+        print(f"Your number is not a whole number.")
+
+else:
+    print("Just enter text consisting of letters. Numbers and signs, as well as spaces, are not accepted")
+    sys.exit()
--- Python3_Basic_Hillel/6_lesson/set.py
+++ Python3_Basic_Hillel/6_lesson/set.py
@@ -3,5 +3,40 @@
 #       - Список літер, які присутні в обох наборах, регістр має значення ("A" та "a" - окремі літери). Літери не повторювати
 #       - Список цифр, які є в першому або в другому наборі, але не в обох. Цифри не повторювати
 
+first_input = input("Writer here your first text: ")
+second_input = input("Writer here your second text: ")
+
+ended_input = set(first_input).union(set(second_input))
+print(f"Your typing has similar characters like this: {list(ended_input)}")
+
+
+ended_set = {el for el in set(first_input).symmetric_difference(set(second_input)) if el.isdigit()}
+
+print(f"A list of numbers, some in the first or the other set, 
+      but not both. Don't repeat the numbers: {list(ended_set)}")
+
+
 # 2.Знайди та ознайомся з операторами |=, &=, -=, ^=, та методами які відповідають цим операторам. Наведи по 
 #   1 прикладу для кожного оператора, використовуй 3 множини одночасно у якості операндів 
+
+a = {1, 2, 3}
+b = {3, 4, 5}
+c = {5, 6, 7}
+
+# Об'єднання множин
+a |= b | c
+print(a)
+
+# Перетин множин
+a &= b & c
+print(a)
+
+# Різниця множин
+a -= b | c
+print(a)
+
+# Симетрична різниця множин
+a ^= b ^ c
+print(a)
+
+
